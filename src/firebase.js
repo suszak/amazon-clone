5a9eaddc4148cd71c5beffef87f6f9dc8a5e6465
suszak <matys.1996@wp.pl>
2020-09-08 23:36:18 +0200

    home page & basket design done

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC5Jsp01SI0V_Hu0GHWHLxyucULB2Hjl68",
+  authDomain: "challange-2d234.firebaseapp.com",
+  databaseURL: "https://challange-2d234.firebaseio.com",
+  projectId: "challange-2d234",
+  storageBucket: "challange-2d234.appspot.com",
+  messagingSenderId: "304483083348",
+  appId: "1:304483083348:web:a154d2dc7de7091462f9c1",
+};
